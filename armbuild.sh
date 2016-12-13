26201ef673de8f89e1a87534ff3f12c938a39de8
BlankRain <731014656@qq.com>
2016-12-13 17:06:26 +0800

    lalalla feeling good

--- armbuild.sh
+++ armbuild.sh
@@ -2,8 +2,8 @@
 #export PATH=/home/abc/source/openwrt/OpenWrt-Toolchain-ramips-rt305x_gcc-5.2.0_musl-1.1.12.Linux-x86_64/toolchain-mipsel_24kec+dsp_gcc-5.2.0_musl-1.1.12/bin:$PATH
 #export PATH=/home/abc/source/openwrt/openwrt-ar9331/staging_dir/toolchain-mips_34kc_gcc-4.8-linaro_uClibc-0.9.33.2/bin:$PATH
 
-export PATH=/home/glove/wrtnode/sdk-ramips-mt7620_gcc4.8/staging_dir/toolchain-mipsel_24kec+dsp_gcc-4.8-linaro_uClibc-0.9.33.2/bin/:$PATH
+export PATH=/home/ecadmin/workerspace/wrt-sdk/staging_dir/toolchain-mipsel_24kec+dsp_gcc-4.8-linaro_uClibc-0.9.33.2/bin/:$PATH
 
-export STAGING_DIR=/home/glove/wrtnode/sdk-ramips-mt7620_gcc4.8/staging_dir/
+export STAGING_DIR=/home/ecadmin/workerspace/wrt-sdk/staging_dir/
 
 ./tools/build.py  $(cat jsconfig.txt) --no-check-test --target-arch=mipsel --target-os=openwrt
